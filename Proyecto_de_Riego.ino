7e0ba8e5f8345289e6affaeb0f30bda775a44b17
Hiraeth248 <glladinom@academia.usbbog.edu.co>
2026-05-23 23:03:31 -0500

    Update Proyecto_de_Riego.ino

--- Proyecto_de_Riego.ino
+++ Proyecto_de_Riego.ino
@@ -1,18 +1,18 @@
-int water; //random variable 
+int water; //Creamos la variable para la humedad a medir 
 void setup() {
-  pinMode(3,OUTPUT); //output pin for relay board, this will sent signal to the relay
-  pinMode(6,INPUT); //input pin coming from soil sensor
+  pinMode(3,OUTPUT); //Decidimos el pin de salida para el relé, encargado de darle señal
+  pinMode(6,INPUT); //El pin de entrada, cuya información vendrá desde el sensor
 }
 
 void loop() { 
-  water = digitalRead(6);  // reading the coming signal from the soil sensor
-  if(water == HIGH) // if water level is full then cut the relay 
+  water = digitalRead(6);  // Leemos la señal proveniente del sensor
+  if(water == HIGH) // Si el nivel del agua es alto, se debe detener el relé
   {
-  digitalWrite(3,LOW); // low is to cut the relay
+  digitalWrite(3,LOW); // Low es para detener el relé
   }
   else
   {
-  digitalWrite(3,HIGH); //high to continue proving signal and water supply
+  digitalWrite(3,HIGH); // High es para continuar la señal y la salida del agua
   }
   delay(400); 
 }
